d83aeddf2d3be9e2efb480f947c9ffaa89c518d9
greenUp <prscustm123@gmail.com>
2016-12-25 15:59:34 -0800

    Added Player class

--- core/src/com/mygdx/game/screens/GameplayScreen.java
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -1,10 +1,8 @@
 package com.mygdx.game.screens;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.mygdx.game.ClickerGame;
 
 public class GameplayScreen extends AbstractScreen{
-	private Texture gameplayImage;
 	
 	public GameplayScreen(ClickerGame game){
 		super(game);
@@ -12,15 +10,10 @@ public class GameplayScreen extends AbstractScreen{
 	}
 	
 	private void init(){
-		gameplayImage = new Texture("badlogic.jpg");
 	}
 	
 	@Override
 	public void render(float delta){
 		super.render(delta);
-		spriteBatch.begin();
-		spriteBatch.draw(gameplayImage,0,0);
-		spriteBatch.end();
-		
 	}
 }
