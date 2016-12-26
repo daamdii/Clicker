1ded08070068729002b9c0078043dca2041c08b4
greenUp <prscustm123@gmail.com>
2016-12-25 16:24:23 -0800

    Displaying Player Image

--- core/src/com/mygdx/game/screens/GameplayScreen.java
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -1,8 +1,10 @@
 package com.mygdx.game.screens;
 
 import com.mygdx.game.ClickerGame;
+import com.mygdx.game.entities.Player;
 
 public class GameplayScreen extends AbstractScreen{
+	private Player player;
 	
 	public GameplayScreen(ClickerGame game){
 		super(game);
@@ -10,10 +12,28 @@ public class GameplayScreen extends AbstractScreen{
 	}
 	
 	private void init(){
+		initPlayer();
+		addPlayerToStage();
 	}
 	
+	private void initPlayer() {
+		player = new Player();
+		
+	}
+	private void addPlayerToStage(){
+		stage.addActor(player);
+	}
+
 	@Override
 	public void render(float delta){
 		super.render(delta);
+		update();
+		spriteBatch.begin();
+		stage.draw();
+		spriteBatch.end();
+	}
+
+	private void update() {
+		stage.act();
 	}
 }
